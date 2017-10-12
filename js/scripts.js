7d2a8a44e1ee7685ac456708a58e467b62be2d92
Your Name <you@example.com>
2017-10-12 12:22:44 +0200

    Add files to project

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/*
+  sample comment
+  console.log('The commented will not be called');
+*/
+var name = prompt('Enter your name');
+// Sample comment about the code in the "alert" line 
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+// end of exercise
